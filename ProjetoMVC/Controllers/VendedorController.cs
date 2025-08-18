a6927738d7cba5f6ac5421202da1d741292a2f63
Guilherme Fiuza Nolasco de Campos <Guilherme.Campos@questor.local>
2025-08-18 16:14:18 -0300

    VendedorController

---
+++ ProjetoMVC/Controllers/VendedorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoMVC.Controllers
+{
+    public class VendedorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
